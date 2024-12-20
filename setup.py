9fe76322c60afdcf139168ded20d1d03d20ff59b
Kituva <kituva.rp02@gmail.com>
2024-12-20 20:33:02 +0530

    First draft

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EarningsCallExpert',
+    version= '0.1',
+    author= 'K R Praveen',
+    author_email= 'kituva.rp02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
